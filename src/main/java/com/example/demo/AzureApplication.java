73f8fafa1515f9dacf74569a95730a936154e9a3
miguelalt64 <48498631+miguelalt64@users.noreply.github.com>
2022-08-28 00:22:46 -0500

    Primer envio

--- src/main/java/com/example/demo/AzureApplication.java
+++ src/main/java/com/example/demo/AzureApplication.java
@@ -16,7 +16,7 @@ public class AzureApplication {
 	@GetMapping("/mensaje")
 	@ResponseStatus(HttpStatus.OK)
 	public String getProfesores(){
-		return "Hola Mundo Azure";
+		return "Hola Mundo Azure!!!!";
 	}
 	
 	public static void main(String[] args) {
